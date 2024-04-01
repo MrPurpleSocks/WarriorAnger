dbda5e71a17afde5f225af8258def1131133bba8
Patrick Woollvin <patrick.woollvin@warriorlife.net>
2024-04-01 01:03:27 -0700

    Match Status added + testing

--- WB-Capture.py
+++ WB-Capture.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 import GetMatches
 import matchStatus
+import testStuff
 
 load_dotenv()
 
@@ -19,6 +20,17 @@ MatchDisplayed = False
 # connects to local OBS instance on port 4454
 cl = obs.ReqClient(host=os.getenv("OBSWS_HOST"), port=os.getenv("OBSWS_PORT"), password=os.getenv("OBSWS_PASSWORD"), timeout=3)
 
+path = os.getenv("ROOT_FOLDER") + "\\" + os.getenv("EVENT_KEY")
+if not os.path.isdir(path):
+    print(path + " Does Not Exist, Creating It")
+    os.mkdir(path)
+else:
+    response = input(path + " Exists, do you want to use it? ")
+    if response == "N" or response == "n":
+        newFolder = input("Please enter a dir name")
+        path = os.getenv("ROOT_FOLDER") + "\\" + newFolder
+        os.mkdir(path)
+    cl.send("SetRecordDirectory", {"recordDirectory": path})
 def signal_handler(sig, frame):
     print("Ctrl+C Pressed!")
     print("Exiting gracefully")
@@ -46,25 +58,33 @@ signal.signal(signal.SIGINT, signal_handler)
 
 try:
     # Gets match keys in order from TBA
-    matches = GetMatches.all(os.getenv("EVENT_KEY"), os.getenv("TBA_TOKEN"))
+    # matches = GetMatches.all(os.getenv("EVENT_KEY"), os.getenv("TBA_TOKEN"))
+    matches = testStuff.getTestMatches()
 
     for i in matches:
-        print("Queuing ", i)
-        if matchStatus.waitForStart():
-            print("Match Started!")
-            start = time.perf_counter()
-            print("Started Recording")
-            cl.toggle_record()
+        cl.set_profile_parameter("Output", "FilenameFormatting", i)
+        print("Match: ", i)
+        print(" ")
+        time.sleep(120)
+        print("Waiting for field to clear")
+        if matchStatus.waitForClear():
+            print("Waiting for match to start")
+            if matchStatus.waitForStart():
+                print("Match Started!")
+                start = time.perf_counter()
+                cl.toggle_record()
+                print("Started Recording")
 
         while True:
             cur_time = time.perf_counter()
-            if round(cur_time-start) == 15 and not AutoDisplayed:
-                print("End of Auto")
-                AutoDisplayed = True
             if round(cur_time-start) == 155 and not MatchDisplayed:
                 print("Match End!")
-                cl.toggle_record()
+                cl.stop_record()
                 MatchDisplayed = True
+                print("Delay to catch errors")
+                time.sleep(30)
+                print(" ")
+                print(" ")
                 break
 
         #reset
--- matchStatus.py
+++ matchStatus.py
@@ -1,7 +1,8 @@
 
 # importing OpenCV, time and Pandas library
-import cv2, time, pandas
-# importing datetime class from datetime library
+import cv2
+import pandas
+import time as tm
 from datetime import datetime
 import numpy
 
@@ -30,11 +31,11 @@ def waitForStart():
 		h, w = plain_frame.shape[:2]
 		mask = numpy.zeros_like(plain_frame)
 		mask[:, :] = [255, 255, 255]
-		mask = cv2.rectangle(mask, (0, h-260), (w, h), (0, 0, 0), -1)
-		mask = cv2.rectangle(mask, (0,0), (w, 120), (0,0,0), -1)
-		mask = cv2.rectangle(mask, (0, 120), (105, h-260), (0,0,0), -1)
-		mask = cv2.rectangle(mask, (w-0, 120), (w-105, 260), (0, 0, 0), -1)
-		mask = cv2.rectangle(mask, ((w//2)-100, 0), ((w//2)+90, h), (0, 0, 0), -1)
+		mask = cv2.rectangle(mask, (0, h-260), (w, h), (0, 0, 0), -1)  # Bottom
+		mask = cv2.rectangle(mask, (0,0), (w, 120), (0,0,0), -1)  # Top
+		mask = cv2.rectangle(mask, (0, 120), (105, h-260), (0,0,0), -1)  # Left
+		mask = cv2.rectangle(mask, (w-0, 120), (w-135, 260), (0, 0, 0), -1)  # Right
+		mask = cv2.rectangle(mask, ((w//2)-100, 0), ((w//2)+90, h), (0, 0, 0), -1) # Middle
 		frame = cv2.bitwise_and(plain_frame, mask)
 
 
@@ -76,6 +77,9 @@ def waitForStart():
 			# making green rectangle around the moving object
 			cv2.rectangle(plain_frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
+		cv2.imshow("Start: Setup", frame)
+		cv2.imshow("Start: Visualize", plain_frame)
+
 		# Appending status of motion
 		motion_list.append(motion)
 
@@ -99,3 +103,120 @@ def waitForStart():
 			if motion == 1:
 				time.append(datetime.now())
 			break
+
+def waitForClear():
+	timerTriggered = False
+	# Assigning our static_back to None
+	static_back = None
+
+	# List when any moving object appear
+	motion_list = [ None, None ]
+
+	# Time of movement
+	time = []
+
+	# Initializing DataFrame, one column is start
+	# time and other column is end time
+	df = pandas.DataFrame(columns = ["Start", "End"])
+
+	# Capturing video
+	video = cv2.VideoCapture(2)
+
+	# Infinite while loop to treat stack of image as video
+	count = 0
+	while True:
+		# Reading frame(image) from video
+		check, plain_frame = video.read()
+
+		h, w = plain_frame.shape[:2]
+		mask = numpy.zeros_like(plain_frame)
+		mask[:, :] = [255, 255, 255]
+		mask = cv2.rectangle(mask, (0, h-250), (w, h), (0, 0, 0), -1)  # Bottom
+		mask = cv2.rectangle(mask, (0,0), (w, 130), (0,0,0), -1)  # Top
+		mask = cv2.rectangle(mask, (0, 120), (105, h), (0,0,0), -1)  # Left
+		mask = cv2.rectangle(mask, (w-0, 120), (w-135, 260), (0, 0, 0), -1)  # Right
+		#mask = cv2.rectangle(mask, ((w//2)-100, 0), ((w//2)+90, h), (0, 0, 0), -1)
+		frame = cv2.bitwise_and(plain_frame, mask)
+
+
+		# Initializing motion = 0(no motion)
+		motion = 0
+
+		# Converting color image to gray_scale image
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		# Converting gray scale image to GaussianBlur
+		# so that change can be find easily
+		gray = cv2.GaussianBlur(gray, (1, 1), 0)
+
+		# In first iteration we assign the value
+		# of static_back to our first frame
+		if static_back is None:
+			static_back = gray
+			continue
+		if count == 50:
+			static_back = gray
+			count = 0
+			continue
+
+		# Difference between static background
+		# and current frame(which is GaussianBlur)
+		diff_frame = cv2.absdiff(static_back, gray)
+
+		# If change in between static background and
+		# current frame is greater than 30 it will show white color(255)
+		thresh_frame = cv2.threshold(diff_frame, 7, 255, cv2.THRESH_BINARY)[1]
+		thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
+
+		# Finding contour of moving object
+		cnts,_ = cv2.findContours(thresh_frame.copy(),
+						cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+		for contour in cnts:
+			if cv2.contourArea(contour) < 2000:
+				continue
+			motion = 1
+
+			(x, y, w, h) = cv2.boundingRect(contour)
+			# making green rectangle around the moving object
+			cv2.rectangle(plain_frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+		if not cnts:
+			motion = 0
+
+		if motion == 0 and not timerTriggered:
+			start = tm.perf_counter()
+			timerTriggered = True
+
+		curTime = tm.perf_counter()
+
+		if timerTriggered and round(curTime - start) == 30:
+			if motion == 0:
+				return True
+			else:
+				timerTriggered = False
+
+		cv2.imshow("Clear: Setup", frame)
+		cv2.imshow("Clear: Visualize", plain_frame)
+
+		# Appending status of motion
+		motion_list.append(motion)
+
+		motion_list = motion_list[-2:]
+
+		# Appending Start time of motion
+		if motion_list[-1] == 1 and motion_list[-2] == 0:
+			time.append(datetime.now())
+
+		# Appending End time of motion
+		if motion_list[-1] == 0 and motion_list[-2] == 1:
+			time.append(datetime.now())
+
+		key = cv2.waitKey(1)
+		# if q entered whole process will stop
+		if key == ord('q'):
+			# if something is movingthen it append the end time of movement
+			if motion == 1:
+				time.append(datetime.now())
+			break
+		count += 1
