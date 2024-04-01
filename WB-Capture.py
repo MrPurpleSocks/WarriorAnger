1c8894ecd3e4a2d8750225b6122246bd155aeff5
Patrick Woollvin <patrick.woollvin@warriorlife.net>
2024-03-31 18:54:55 -0700

    updated WB-Capture.py

--- WB-Capture.py
+++ WB-Capture.py
@@ -1,5 +1,7 @@
 import os
 import time
+import signal
+import sys
 
 import obsws_python as obs
 from dotenv import load_dotenv
@@ -11,23 +13,62 @@ load_dotenv()
 
 # counts number of recordings made
 numFiles = 0
-
-# Gets match keys in order from TBA
-matches = GetMatches.all(os.getenv("EVENT_KEY"), os.getenv("TBA_TOKEN"))
+AutoDisplayed = False
+MatchDisplayed = False
 
 # connects to local OBS instance on port 4454
 cl = obs.ReqClient(host=os.getenv("OBSWS_HOST"), port=os.getenv("OBSWS_PORT"), password=os.getenv("OBSWS_PASSWORD"), timeout=3)
 
-for i in matches:
-    print("Match Queuing")
-    if matchStatus.waitForStart():
-        print("Match Started!")
-        start = time.perf_counter()
-        print("Started timer")
-    while True:
-        cur_time = time.perf_counter()
-        print(cur_time-start)
-        if cur_time-start == 15:
-            print("End of Auto")
-        if cur_time-start == 150:
-            print("Match End!")
+def signal_handler(sig, frame):
+    print("Ctrl+C Pressed!")
+    print("Exiting gracefully")
+    cl.set_current_program_scene("brb")
+    time.sleep(1)
+    if resp.output_active:
+        cl.stop_record()
+        print("Stopped Recording")
+    else:
+        print("OBS Wasn't recording")
+    sys.exit(0)
+def error_handler():
+    print("Code Exited Code 1!")
+    print("Exiting gracefully")
+    cl.set_current_program_scene("brb 2")
+    time.sleep(1)
+    resp = cl.get_record_status()
+    if resp.output_active:
+        cl.stop_record()
+        print("Stopped Recording")
+    else:
+        print("OBS Wasn't recording")
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    # Gets match keys in order from TBA
+    matches = GetMatches.all(os.getenv("EVENT_KEY"), os.getenv("TBA_TOKEN"))
+
+    for i in matches:
+        print("Queuing ", i)
+        if matchStatus.waitForStart():
+            print("Match Started!")
+            start = time.perf_counter()
+            print("Started Recording")
+            cl.toggle_record()
+
+        while True:
+            cur_time = time.perf_counter()
+            if round(cur_time-start) == 15 and not AutoDisplayed:
+                print("End of Auto")
+                AutoDisplayed = True
+            if round(cur_time-start) == 155 and not MatchDisplayed:
+                print("Match End!")
+                cl.toggle_record()
+                MatchDisplayed = True
+                break
+
+        #reset
+        MatchDisplayed = False
+        AutoDisplayed = False
+except:
+    error_handler()
